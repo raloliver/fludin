6aadd5395e2454f8176a5136403b6a59cb1bd656
raloliver <contato@raloliver.com.br>
2020-06-12 11:17:36 +0100

    Before add interaction button in our app

--- fludin03/README.md
+++ fludin03/README.md
@@ -6,4 +6,5 @@ The documentaion is our friend.
 
 - AssetImage(): put a image from image folder. Just specify a string with a image path. Remember: this is a method from prop image in a Image() widget.
 - Expanded(): fill the avaliable space. He widget is a similar to a flex layout in a web way.
-- 
+
+> Flutter Outline in a Android Studio it is a great tool to improve your productivity.
--- fludin03/lib/main.dart
+++ fludin03/lib/main.dart
@@ -18,21 +18,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          flex: 2,
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
           ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Image(
-            image: AssetImage('images/dice2.png'),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice2.png'),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
