795ad67401500a3f056acefb6393465b7c99d6d3
raloliver <contato@raloliver.com.br>
2020-06-12 10:33:02 +0100

    Initfludin0

--- fludin03/test/widget_test.dart
+++ fludin03/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fludin01/main.dart';
+import 'package:fludin03/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
